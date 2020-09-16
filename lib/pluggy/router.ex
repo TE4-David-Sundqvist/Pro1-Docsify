a1cbf9ca5c2936e775440aff7cfb7c6e234c744b
Dennis-coder <54890763+Dennis-coder@users.noreply.github.com>
2020-09-16 13:34:37 +0200

    hej

--- lib/pluggy/router.ex
+++ lib/pluggy/router.ex
@@ -26,21 +26,9 @@ defmodule Pluggy.Router do
   plug(:match)
   plug(:dispatch)
 
-  get("/fruits", do: FruitController.index(conn))
-  get("/fruits/new", do: FruitController.new(conn))
-  get("/fruits/:id", do: FruitController.show(conn, id))
-  get("/fruits/:id/edit", do: FruitController.edit(conn, id))
+  get("/login", do: LoginController.index(conn))
 
-  post("/fruits", do: FruitController.create(conn, conn.body_params))
-
-  # should be put /fruits/:id, but put/patch/delete are not supported without hidden inputs
-  post("/fruits/:id/edit", do: FruitController.update(conn, id, conn.body_params))
-
-  # should be delete /fruits/:id, but put/patch/delete are not supported without hidden inputs
-  post("/fruits/:id/destroy", do: FruitController.destroy(conn, id))
-
-  post("/users/login", do: UserController.login(conn, conn.body_params))
-  post("/users/logout", do: UserController.logout(conn))
+  post("/login", do: LoginController.login(conn, conn.body_params))
 
   match _ do
     send_resp(conn, 404, "oops")
